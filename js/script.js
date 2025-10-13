1b99c5172e4c51f3012cffa85fac7b34821babbd
Hiromu-Chiba118 <108908014+Hiromu-Chiba118@users.noreply.github.com>
2025-10-13 17:40:37 +0900

    タブ切り替え

--- js/script.js
+++ js/script.js
@@ -46,4 +46,17 @@ jQuery(function ($) {
     $('.drawer-menu').toggleClass('open');
     $('html').toggleClass('is-fixed');
   });
+
+  // タブ切り替え機能
+  $('.top-service__tab').on('click', function () {
+    var targetTab = $(this).attr('data-tab');
+
+    // タブのアクティブ状態を切り替え
+    $('.top-service__tab').removeClass('top-service__tab--active');
+    $(this).addClass('top-service__tab--active');
+
+    // パネルの表示を切り替え
+    $('.top-service__panel').removeClass('top-service__panel--active');
+    $('.top-service__panel[data-panel="' + targetTab + '"]').addClass('top-service__panel--active');
+  });
 });
